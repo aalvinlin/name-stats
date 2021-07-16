4a6f3df45eaa190f2cb1dc855d62a2e6822532c9
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2021-07-16 14:59:26 -0700

    add state to NameForm component; change to a controlled component

--- src/components/NameForm.js
+++ src/components/NameForm.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const NameForm = () => {
+
+    const [userInput, setUserInput] = useState("");
+
+    const handleNameInput = event => {
+        setUserInput(event.target.value);
+    }
+
     return (
         <div id="nameForm">
             <h2>Type your name to begin...</h2>
             <form>
-                <input name="name_input"></input>
+                <input name="name_input" onInput={handleNameInput} value={userInput}></input>
             </form>
         </div>
     )
