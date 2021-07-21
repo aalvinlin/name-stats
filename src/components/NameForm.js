c91450cfb14a92b8855cf48c58d3ff5f96635e5a
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2021-07-20 23:57:39 -0700

    add button to name form

--- src/components/NameForm.js
+++ src/components/NameForm.js
@@ -9,11 +9,16 @@ export const NameForm = () => {
         setUserInput(event.target.value);
     }
 
+    const handleSubmit = event => {
+        
+    }
+
     return (
         <div id="nameForm">
             <h2>Type your name to begin...</h2>
             <form>
                 <input name="name_input" onInput={handleNameInput} value={userInput}></input>
+                <button name="name_button" onClick={handleSubmit}>Go!</button>
             </form>
         </div>
     )
